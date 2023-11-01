805d729d8193c6b67f98243ddcf538b0f12611d4
chemoiko <129403454+chemoiko@users.noreply.github.com>
2023-11-01 08:55:36 +0300

    Update numberone.py

--- numberone.py
+++ numberone.py
@@ -1,6 +1,6 @@
-print("Elijah am the new")
-print("Hello Elijah have been added")
-print(" Elijah am the newest")
+print("Elijah and Ronnie  am the new")
+print("Hello Elijah and Ronnie have been added")
+print(" Elijah and Ronnie am the newest")
 
 
 # i am for master
